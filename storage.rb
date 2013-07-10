37b353b883ab9bbdb5163249f37d61f65639f1c2
James Thullbery <jthullbery@teladoc.com>
2013-07-10 07:49:23 -0400

    added index.html page to use YUI to make calls to storage.rb

--- storage.rb
+++ storage.rb
@@ -1,6 +1,33 @@
 require 'sinatra'
 require 'yajl/json_gem'
 
+configure do 
+  mime_type :javascript, 'text/javascript'
+end
+
+get '/' do
+  content_type :javascript
+    data = {query: 
+      [{ id: "ga-3475", name: "gadget",   price: "$6.99", cost: "$5.99" },
+             { id: "sp-9980", name: "sprocket", price: "$3.75", cost: "$3.25" },
+             { id: "wi-0650", name: "widget",   price: "$4.25", cost: "$3.75" }] 
+    }.to_json
+
+end
+
+
+
+get '/test' do
+  content_type :javascript
+    data = {query: 
+      [{ id: "ga-3475", name: "gadget",   price: "$6.99", cost: "$5.99" },
+             { id: "sp-9980", name: "sprocket", price: "$3.75", cost: "$3.25" },
+             { id: "wi-0650", name: "widget",   price: "$4.25", cost: "$3.75" }] 
+    }.to_json
+
+end
+
+
 get '/location' do
   {name: 'Billings', location: '45.7833N, 108.5000W'}.to_json
 end
